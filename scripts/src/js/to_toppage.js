4cfbcf05efa002ae64c4d6722e6e26c266bf1646
sake <sakelog.website@gmail.com>
2020-10-25 14:10:03 +0900

    to_toppage.js

--- scripts/src/js/to_toppage.js
+++ scripts/src/js/to_toppage.js
@@ -1,17 +1,13 @@
-var target = document.getElementById('to_page_top');
-var duration = 500;
-target.addEventListener('click', function () {
-    var currentY = window.pageYOffset;
-    var step = duration / currentY > 1 ? 10 : 100;
-    var timeStep = duration / currentY * step;
-    var intervalID = setInterval(scrollUP, timeStep);
-    function scrollUP() {
-        currentY = window.pageYOffset;
-        if (currentY === 0) {
-            clearInterval(intervalID);
-        }
-        else {
-            scrollBy(0, -step);
-        }
-    }
-});
+function getScrolled() {
+    return window.pageYOffset !== undefined
+        ? window.pageYOffset
+        : document.documentElement.scrollTop;
+}
+var topbutton = document.getElementById('to_page_top');
+function scrollToTop() {
+    var scrolled = getScrolled();
+    window.scrollTo(0, 0);
+}
+topbutton.onclick = function () {
+    scrollToTop();
+};
