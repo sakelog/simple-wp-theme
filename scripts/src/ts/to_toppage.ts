31aaa9132d95d773a1e48ffdf1e5481ad96e8ad3
sake <sakelog.website@gmail.com>
2020-10-25 14:40:50 +0900

    totoppage.js

--- scripts/src/ts/to_toppage.ts
+++ scripts/src/ts/to_toppage.ts
@@ -1,11 +1,5 @@
-function getScrolled() {
-  return window.pageYOffset !== undefined
-    ? window.pageYOffset
-    : document.documentElement.scrollTop;
-}
 var topbutton = document.getElementById('to_page_top');
 function scrollToTop() {
-  var scrolled = getScrolled();
   window.scrollTo(0, 0);
 }
 topbutton.onclick = function () {
